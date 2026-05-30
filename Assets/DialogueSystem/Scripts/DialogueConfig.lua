37ff4e03cfcd42b828e520c0fe51927ac03e3f9a
GuanYao <61416718+2625580908@users.noreply.github.com>
2026-05-31 00:48:52 +0800

    更新测试场景 NPC 对话
    更新测试场景 NPC 对话

--- Assets/DialogueSystem/Scripts/DialogueConfig.lua
+++ Assets/DialogueSystem/Scripts/DialogueConfig.lua
@@ -1,15 +1,21 @@
 DialogueConfig={}
 
-DialogueConfig[1]={RoleLeft="Npc1",RoleRight="",LeftName="Name_1",RightName="",Dialogue="对话1。",Next=2}
-DialogueConfig[2]={RoleLeft="",RoleRight="Npc2",LeftName="",RightName="Name_2",Dialogue="对话2。",Next=3}
-DialogueConfig[3]={RoleLeft="Npc1",RoleRight="",LeftName="Name_1",RightName="",Dialogue="对话3。",Next=-1}
+DialogueConfig[1]={RoleLeft="Npc2",RoleRight="",LeftName="描述",RightName="",Dialogue="鸡舍门口，一只母鸡正站着，忽然抬起头来",Next=2}
+DialogueConfig[2]={RoleLeft="",RoleRight="Npc3",LeftName="",RightName="淑芬",Dialogue="哎——你等一下！",Next=3}
+DialogueConfig[3]={RoleLeft="",RoleRight="Npc3",LeftName="",RightName="淑芬",Dialogue="帮个忙行不行？我找不到孩儿了——",Next=4}
 
-DialogueConfig[4]={RoleLeft="",RoleRight="Npc3",LeftName="",RightName="Name_3",Dialogue="对话4。",Next=5}
-DialogueConfig[5]={RoleLeft="Npc4",RoleRight="",LeftName="Name_4",RightName="",Dialogue="对话5。",Next=6}
-DialogueConfig[6]={RoleLeft="",RoleRight="Npc3",LeftName="",RightName="Name_3",Dialogue="对话6。",Next=7}
-DialogueConfig[7]={RoleLeft="Npc4",RoleRight="",LeftName="Name_4",RightName="",Dialogue="对话7。",Next=-1}
+DialogueConfig[4]={RoleLeft="Npc2",RoleRight="",LeftName="描述",RightName="",Dialogue="往鸡窝方向领了一步，又回头张望",Next=5}
 
-DialogueConfig[8]={RoleLeft="",RoleRight="Npc1",LeftName="",RightName="Name_1",Dialogue="对话8。",Next=9}
-DialogueConfig[9]={RoleLeft="Npc3",RoleRight="",LeftName="Name_3",RightName="",Dialogue="对话9。",Next=10}
-DialogueConfig[10]={RoleLeft="",RoleRight="Npc1",LeftName="",RightName="Name_1",Dialogue="对话10。",Next=11}
-DialogueConfig[11]={RoleLeft="Npc3",RoleRight="",LeftName="Name_3",RightName="",Dialogue="对话10。",Next=-1}
+DialogueConfig[5]={RoleLeft="",RoleRight="Npc3",LeftName="",RightName="淑芬",Dialogue="画了爱心标记的那颗蛋，今早发现没了。",Next=6}
+DialogueConfig[6]={RoleLeft="",RoleRight="Npc3",LeftName="",RightName="淑芬",Dialogue="其他孩子出壳都好几天了，就这最小的——迟迟没动静，我一直守着它。",Next=7}
+DialogueConfig[7]={RoleLeft="",RoleRight="Npc3",LeftName="",RightName="淑芬",Dialogue="昨晚前还动过一下，那种感觉……我不可能认错的。",Next=8}
+
+DialogueConfig[8]={RoleLeft="Npc2",RoleRight="",LeftName="描述",RightName="",Dialogue="停了停，看了看天",Next=9}
+
+DialogueConfig[9]={RoleLeft="",RoleRight="Npc3",LeftName="",RightName="淑芬",Dialogue="最近天骤然凉了，我就担心——",Next=10}
+DialogueConfig[10]={RoleLeft="",RoleRight="Npc3",LeftName="",RightName="淑芬",Dialogue="主人也来看了好几回，来的比我还频繁，就那样站着不说话。",Next=11}
+DialogueConfig[11]={RoleLeft="",RoleRight="Npc3",LeftName="",RightName="淑芬",Dialogue="你帮我找找吧，行吗？",Next=12}
+
+DialogueConfig[12]={RoleLeft="",RoleRight="Npc1",LeftName="",RightName="玩家",Dialogue="好，我来帮你找找。",Next=13}
+DialogueConfig[13]={RoleLeft="",RoleRight="Npc3",LeftName="",RightName="淑芬",Dialogue="谢谢你，真的。有什么要问的随时来找我——农场的事，我比谁都熟。",Next=-1}
+
